e57b0ef061568a2b7491cd6d407961a622796ace
Michel Van Autrève <michel.vanautreve@outlook.com>
2021-12-23 05:44:30 +0100

    update

--- js/globe.js
+++ js/globe.js
@@ -1,5 +1,5 @@
-//const hData = [];
 const api = "https://api.spacexdata.com/v4/starlink";
+let world;
 
 const getData = async (url) => {
   const response = await fetch(url);
@@ -7,36 +7,49 @@ const getData = async (url) => {
   return json;
 };
 
-const convertData = async () => {
+const zoom = (e) => {
+  console.log(e);
+  world.pointOfView({ lat: e.lat, lng: e.lng, altitude: 0.5 }, 1000);
+  world.controls().autoRotate = false;
+  document.querySelector(`.nameInput`).textContent = e.name;
+  document.querySelector(`.launchInput`).textContent = e.launch;
+  document.querySelector(`.latInput`).textContent = e.lat;
+  document.querySelector(`.longInput`).textContent = e.long;
+  document.querySelector(`.altInput`).textContent = e.alt * 5000;
+};
+
+const createGlobe = async () => {
   const satellites = [];
+
   const data = await getData(api);
+
   data.forEach((element) => {
     if (
       element.latitude != null &&
       element.longitude != null &&
       element.height_km != null &&
-      element.velocity_kms != null
+      element.velocity_kms != null &&
+      element.spaceTrack.OBJECT_NAME != null &&
+      element.spaceTrack.LAUNCH_DATE != null
     ) {
       const satellite = {
+        name: element.spaceTrack.OBJECT_NAME,
         lat: element.latitude,
         lng: element.longitude,
-        alt: element.height_km / 5000,
+        alt: element.height_km,
         radius: 0.5,
         color: "crimson",
-        spd: element.velocity_kms / 1000,
+        spd: element.velocity_kms,
+        launch: element.spaceTrack.LAUNCH_DATE,
       };
       satellites.push(satellite);
     }
   });
 
-  const zoom = (e) => {
-    console.log(e);
-    e.toGeoCoords({ lat: 2, lng: 4, altitude: 1 });
-  };
-
-  const world = Globe()(document.querySelector(".globe"))
+  world = Globe()(document.querySelector(".c-globe"))
     .globeImageUrl("//unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
     .bumpImageUrl("//unpkg.com/three-globe/example/img/earth-topology.png")
+    .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png")
     .pointOfView({ altitude: 1.5 })
     .customLayerData(satellites)
     .onCustomLayerClick(zoom)
@@ -48,31 +61,18 @@ const convertData = async () => {
         )
     )
     .customThreeObjectUpdate((obj, d) => {
-      Object.assign(obj.position, world.getCoords(d.lat, d.lng, d.alt));
+      Object.assign(obj.position, world.getCoords(d.lat, d.lng, d.alt / 5000));
     });
 
-  //world.controls().autoRotate = true;
-  //world.controls().autoRotateSpeed = -1;
+  world.controls().autoRotate = true;
+  world.controls().autoRotateSpeed = 0.1;
 
-  (function moveSpheres() {
-    satellites.forEach((d) => (d.lng += d.spd));
-    world.customLayerData(world.customLayerData());
-    requestAnimationFrame(moveSpheres);
-  })();
+  document.querySelector(`.rotationInput`).addEventListener("input", (e) => {
+    world.controls().autoRotateSpeed = e.target.value;
+    world.controls().autoRotate = true;
+  });
 };
 
 document.addEventListener(`DOMContentLoaded`, () => {
-  convertData();
+  createGlobe();
 });
-
-const N = 300;
-const gData = [...Array(N).keys()].map(() => ({
-  lat: (Math.random() - 0.5) * 180,
-  lng: (Math.random() - 0.5) * 360,
-  alt: Math.random() * 0.8 + 0.1,
-  radius: Math.random() * 5,
-  color: ["#586BDB", "#8C51E8", "#F2441D", "#262626"][
-    Math.round(Math.random() * 3)
-  ],
-}));
-//console.log(hData);
